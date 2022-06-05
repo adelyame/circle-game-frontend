4eae4fcef9059bbb42e6f9ee318e30e944f18ef0
Adelya <adelya19992989@gmail.com>
2022-06-05 20:26:33 +0500

    some changes

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 let elem = document.getElementById("circle");
+let parentElem = document.getElementById("parent");
+
 elem.tabIndex = 0;
 
-elem.addEventListener("keydown", function (event) {
-    if (event.code != "ArrowRight" && event.code != "ArrowLeft" &&
-        event.code != "ArrowUp" && event.code != "ArrowDown") return;
-    let rectElem = elem.getBoundingClientRect();
-    let x = rectElem.x + pageXOffset,
-        y = rectElem.y + pageYOffset;
+document.body.style.overflow = "hidden"; // избавление от эффекта прокрутки
 
-    if (event.code == "ArrowRight") x += elem.offsetWidth;
-    if (event.code == "ArrowLeft")  x -= elem.offsetWidth;
-    if (event.code == "ArrowUp")    y -= elem.offsetHeight;
-    if (event.code == "ArrowDown")  y += elem.offsetHeight;
-    
+
+
+elem.addEventListener("keydown", function (event)
+{
+
+    if (event.code != "ArrowRight" && event.code != "ArrowLeft" &&
+        event.code != "ArrowUp" && event.code != "ArrowDown") {
+
+            return;
+    }
+
+    let rectElem = elem.getBoundingClientRect();
+
+    let x = rectElem.x + scrollX;
+    let y = rectElem.y + scrollY;
+
+    // if (event.code == "ArrowRight") x += elem.offsetWidth;
+    // if (event.code == "ArrowLeft")  x -= elem.offsetWidth;
+    // if (event.code == "ArrowUp")    y -= elem.offsetHeight;
+    // if (event.code == "ArrowDown")  y += elem.offsetHeight;
+
+    if (event.code == "ArrowDown" && event.code == "ArrowRight")  {
+
+        console.log("vasya");
+        y += 10;
+        x -= 10;
+    }
+
+    if (event.code == "ArrowRight") {
+        x += 10;
+    } 
+    if (event.code == "ArrowLeft")  x -= 10;
+    if (event.code == "ArrowUp")    y -= 10;
+    if (event.code == "ArrowDown")  y += 10;
+
+
     elem.style.position = "absolute";
     elem.style.left = x + "px";
     elem.style.top = y + "px";
 });
+
+// console.log(parentElem.style.width)
